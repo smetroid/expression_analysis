bc4ea1056cbe3700995d0fcb933c1d09b269dc03
EnriqueC <enriquegc1982@gmail.com>
2018-10-27 19:08:46 -0500

    working data

--- expression_analysis.py
+++ expression_analysis.py
@@ -4,14 +4,22 @@ import sys
 
 
 def createDBs(conn):
-    interproscan7_table = "create table interproscan7(trinity, random1, random2, sites, code1, description1, start, stop, evalue, random3, date, code2, description2, goterms, reactome)"
+    interproscan7_table     = "create table interproscan7(trinity, random1, random2, sites, code1, description1, start, stop, evalue, random3, date, code2, description2, goterms, reactome)"
     expression_counts_table = "create table expression_counts (trinity, ho8_quants, ho7_quants)"
-    fasta                   = "create table fasta (trinity, data BLOB)"
+    interproscan8_table     = "create table interproscan8(trinity, random1, random2, sites, code1, description1, start, stop, evalue, random3, date, code2, description2, goterms, reactome)"
+    fastaho7                = "create table fastaho7 (trinity, data BLOB)"
+    fastaho8                = "create table fastaho8 (trinity, data BLOB)"
+    fastaho7transdecoder    = "create table fastaho7transdecoder (trinity, data BLOB)"
+    fastaho8transdecoder    = "create table fastaho8transdecoder (trinity, data BLOB)"
 
     cur = conn.cursor()
     cur.execute(interproscan7_table)
+    cur.execute(interproscan8_table)
     cur.execute(expression_counts_table)
-    cur.execute(fasta)
+    cur.execute(fastaho7)
+    cur.execute(fastaho8)
+    cur.execute(fastaho7transdecoder)
+    cur.execute(fastaho8transdecoder)
 
 
 def testDB(cur):
@@ -24,12 +32,12 @@ def testDB(cur):
 
 
 def loadInterproScan7(cur):
-    tsv_data_file = open(sys.argv[1])
+    tsv_data_file = open("./data/Ho7_K31_Trinity_InterProScan_1.tsv")
     tsv_reader = csv.reader(tsv_data_file, delimiter="\t")
 
     for i in tsv_reader:
         # Remove the .p1 from the trinity value
-        i[0] = re.sub(r'_i.*$', '', i[0])
+        #i[0] = re.sub(r'_i.*$', '', i[0])
         print(i)
         if len(i) < 15:
             continue
@@ -38,8 +46,26 @@ def loadInterproScan7(cur):
             cur.execute("INSERT INTO interproscan7 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", tuple(i))
 
 
+def loadInterproScan8(cur):
+    tsv_data_file = open("./data/Ho8_k31_Trinity_InterProScan.tsv")
+    tsv_reader = csv.reader(tsv_data_file, delimiter="\t")
+
+    for i in tsv_reader:
+        # Remove the .p1 from the trinity value
+        #i[0] = re.sub(r'_i.*$', '', i[0])
+        print(i)
+        if len(i) < 15:
+            continue
+            cur.execute("INSERT INTO interproscan8 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", tuple(i))
+        else:
+            cur.execute("INSERT INTO interproscan8 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", tuple(i))
+
+
+
+
+
 def loadNorm(cur):
-    tsv_data_file = open(sys.argv[2])
+    tsv_data_file = open("./norm")
     tsv_reader = csv.reader(tsv_data_file, delimiter="\t")
 
     for i in tsv_reader:
@@ -54,7 +80,7 @@ def loadNorm(cur):
 
 def getDataSet(cur, filter):
     cur.execute("SELECT trinity,start,stop FROM interproscan7 WHERE code1 LIKE ?", (filter,))
-    file_name = "%s_%s.csv" % (sys.argv[2], filter)
+    file_name = "%s_%s.csv" % ("interproscan7", filter)
     rows = cur.fetchall()
     data = []
     fo = open(file_name, 'w')
@@ -88,7 +114,7 @@ def getNormAndHoData(cur, filter):
                 'WHERE code1 LIKE "%s" '
                 'ORDER BY inter.trinity ')
 
-    file_name = "%s_%s.csv" % (sys.argv[1],filter)
+    file_name = "%s_%s.csv" % ("normAndHoData",filter)
     print sql % (filter)
     cur.execute(sql % (filter))
     rows = cur.fetchall()
@@ -105,29 +131,108 @@ def getNormAndHoData(cur, filter):
     fo.close()
 
 
-def loadFastaData(cur):
+def loadFastaDataHo7(cur):
     trinity = ""
     data = ""
-    with open(sys.argv[3]) as f:
+    with open("./data/Ho7_K31_Trinity.fasta") as f:
         for i in f:
             match = re.search(r'^>TRINITY', i)
             #print i
             if (match):
                 if (data != ""):
                     sql_insert = (trinity, data)
-                    cur.execute("INSERT INTO fasta VALUES (?, ?)", tuple(sql_insert))
+                    cur.execute("INSERT INTO fastaho7 VALUES (?, ?)", tuple(sql_insert))
                     data = i
                 else:
                     data = i
 
                 trinity_field = i.split(" ")
-                trinity = re.sub(r'_i.*$', '', trinity_field[0]).replace('>', '')
+                #trinity = re.sub(r'_i.*$', '', trinity_field[0]).replace('>', '')
+                trinity = trinity_field[0].replace('>', '')
             else:
                 data += i
 
         # When EOF is reached commit the last values into SQL
         sql_insert = (trinity, data)
-        cur.execute("INSERT INTO fasta VALUES (?, ?)", tuple(sql_insert))
+        cur.execute("INSERT INTO fastaho7 VALUES (?, ?)", tuple(sql_insert))
+
+
+def loadFastaDataHo8(cur):
+    trinity = ""
+    data = ""
+    with open("./data/Ho8_K31_Trinity.fasta") as f:
+        for i in f:
+            match = re.search(r'^>TRINITY', i)
+            #print i
+            if (match):
+                if (data != ""):
+                    sql_insert = (trinity, data)
+                    cur.execute("INSERT INTO fastaho8 VALUES (?, ?)", tuple(sql_insert))
+                    data = i
+                else:
+                    data = i
+
+                trinity_field = i.split(" ")
+                #trinity = re.sub(r'_i.*$', '', trinity_field[0]).replace('>', '')
+                trinity = trinity_field[0].replace('>', '')
+            else:
+                data += i
+
+        # When EOF is reached commit the last values into SQL
+        sql_insert = (trinity, data)
+        cur.execute("INSERT INTO fastaho8 VALUES (?, ?)", tuple(sql_insert))
+
+
+def loadFastaDataHo7Transdecoder(cur):
+    trinity = ""
+    data = ""
+    with open("./data/Ho7_K31_Trinity.fasta.transdecoder.pep") as f:
+        for i in f:
+            match = re.search(r'^>TRINITY', i)
+            #print i
+            if (match):
+                if (data != ""):
+                    sql_insert = (trinity, data)
+                    cur.execute("INSERT INTO fastaho7transdecoder VALUES (?, ?)", tuple(sql_insert))
+                    data = i
+                else:
+                    data = i
+
+                trinity_field = i.split(" ")
+                #trinity = re.sub(r'_i.*$', '', trinity_field[0]).replace('>', '')
+                trinity = trinity_field[0].replace('>', '')
+            else:
+                data += i
+
+        # When EOF is reached commit the last values into SQL
+        sql_insert = (trinity, data)
+        cur.execute("INSERT INTO fastaho7transdecoder VALUES (?, ?)", tuple(sql_insert))
+
+
+def loadFastaDataHo8Transdecoder(cur):
+    trinity = ""
+    data = ""
+    with open("./data/Ho8_K31_Trinity.fasta.transdecoder.pep") as f:
+        for i in f:
+            match = re.search(r'^>TRINITY', i)
+            #print i
+            if (match):
+                if (data != ""):
+                    sql_insert = (trinity, data)
+                    cur.execute("INSERT INTO fastaho8transdecoder VALUES (?, ?)", tuple(sql_insert))
+                    data = i
+                else:
+                    data = i
+
+                trinity_field = i.split(" ")
+                #trinity = re.sub(r'_i.*$', '', trinity_field[0]).replace('>', '')
+                trinity = trinity_field[0].replace('>', '')
+            else:
+                data += i
+
+        # When EOF is reached commit the last values into SQL
+        sql_insert = (trinity, data)
+        cur.execute("INSERT INTO fastaho8transdecoder VALUES (?, ?)", tuple(sql_insert))
 
 
 def fastaDataQuery(cur):
@@ -150,6 +255,7 @@ if __name__ == "__main__":
     createDBs(conn)
     cur = conn.cursor()
     loadInterproScan7(cur)
+    loadInterproScan8(cur)
     loadNorm(cur)
 
     # PF00201
@@ -159,7 +265,10 @@ if __name__ == "__main__":
     buildTempView(cur)
     getNormAndHoData(cur, "PF00201")
     getNormAndHoData(cur, "PS00375")
-    loadFastaData(cur)
+    loadFastaDataHo7(cur)
+    loadFastaDataHo8(cur)
+    loadFastaDataHo7Transdecoder(cur)
+    loadFastaDataHo8Transdecoder(cur)
 
 
     conn.commit()
